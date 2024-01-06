a213407b7035c64df20a5f393a8f05859e55f1cb
Evan Simmons <47922276+evansimm@users.noreply.github.com>
2024-01-06 15:10:44 -0500

    added comments and filled in basic readme
    added comments and filled in basic readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# passwordgenerator
-A python script that generates a secure alphanumeric password.
+# Password Generator
+The Password Generator is a simple yet effective Python script for creating secure, random passwords. Designed for ease of use, it automatically generates passwords containing a mix of uppercase and lowercase letters, digits, and special characters.
+
+## License
+Distributed under the MIT License. See LICENSE for more information.
--- passwordgenerator.py
+++ passwordgenerator.py
@@ -2,15 +2,19 @@
 
 import random
 
+# Character sets for the password
 capitalLetters = "QWERTYUIOPASDFGHJKLZXCVBNM"
 lowerLetters = "qwertyuiopasdfghjklzxcvbnm"
 numbers = "1234567890"
 specialCharacters = "?,.<>!%$[]}{*)(|@#%^&-_=+"
 
+# Set the character sets to be used
 capital,lower,digits,special = True,True,True,True
 
+# Initialize the final string of characters to be used in the password
 finalPassword = ""
 
+# Add the character sets to the final string of characters
 if capital:
     finalPassword += capitalLetters
 if lower:
@@ -20,9 +24,11 @@ if digits:
 if special:
     finalPassword += specialCharacters
 
+# Set the length and quantity of the password
 passwordLength = 16
 passwordQuantity = 10
 
+# Create the password then print it
 for x in range(passwordQuantity):
     password = "".join(random.sample(finalPassword,passwordLength))
     print(password)
